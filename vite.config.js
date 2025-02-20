f0adec156235c9ad46e0c7c12ed179d0b1cca8aa
NofaceNocase-gif <charlesomu6@gmail.com>
2025-02-20 06:26:23 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://healthy-you-be.onrender.com',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+});
+
